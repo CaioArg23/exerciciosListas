ec520ccee8d05375757fe85453fe19c1d2d30e69
CaioArg23 <caio21lf@gmail.com>
2025-02-13 17:45:43 -0300

    segundo commit

---
+++ exercicios12.02/manipulandoListas/extend().py
@@ -0,0 +1,4 @@
+#o extend vai adicionar VÁRIOS elementos ao final de uma lista. Ex:
+lista = [9, 10, 11, 12, 13]
+lista.extend([14, 15])
+print (lista)
---
+++ exercicios12.02/manipulandoListas/remove()
@@ -0,0 +1,4 @@
+#o remove() é utlizado para remover um item por vez em uma lista. Ex:
+lista = [9, 10, 11, 12, 13]
+lista.remove(9)
+print (lista)
