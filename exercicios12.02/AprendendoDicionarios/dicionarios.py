45a84a2734c68c38966965206357d06f9d4f0365
CaioArg23 <caio21lf@gmail.com>
2025-02-14 11:23:02 -0300

    quarto commit

--- exercicios12.02/AprendendoDicionarios/dicionarios.py
+++
@@ -1,8 +0,0 @@
-#Os dicionários são um tipo de estrutura de dados que armazenam pares de chave-valor. Eles são delimitados por chaves {} 
-#e os pares chave-valor são separados por vírgulas. Ex:
-dicionario = {
-    'chave1': 1,
-    'chave2': 2
-} 
-print (dicionario)
-
--- exercicios12.02/manipulandoListas/len().py
+++
@@ -1,3 +0,0 @@
-#para contar a quantidades de elementos em uma lista pode se utilizar a função len().  Ex:
-nomes = ['caio', 'daniel', 'henry', 'lucas', 'mateus', 4, 5]
-print (len(nomes))
